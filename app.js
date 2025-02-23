797014250c819561b9076e60593f8c590a569cd5
burhanbitkraft <burhanuddin@bitkraft.co.in>
2025-02-23 22:29:43 +0530

    changes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://devconnects.xyz",
+    origin: "https://devconnects.xyz/api",
     credentials: true,
   })
 );
