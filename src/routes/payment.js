5d79e1e1af40554738a80233fd1d72e1664d6137
burhanbitkraft <burhanuddin@bitkraft.co.in>
2025-02-26 19:24:10 +0530

    changes in payment file

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -33,6 +33,9 @@ router.post("/payment/create", userAuth, async (req, res) => {
       receipt: order.receipt,
       notes: order.notes,
     });
+
+    await savedPayment.save();
+
     res.status(200).json({
       success: true,
       message: "order created Successfully",
@@ -51,11 +54,18 @@ router.post("/payment/webhook", async (req, res) => {
   const paymentDetails = req.body.payload.payment.entity;
   const webhookSignature = req.get("X-Razorpay-Signature");
 
+  console.log("reaching Headers", paymentDetails, webhookSignature);
+  console.log(
+    "process.env.RAZOR_PAY_WEBHOOK_KEY",
+    process.env.RAZOR_PAY_WEBHOOK_KEY
+  );
+
   const isWebHookValid = validateWebhookSignature(
     JSON.stringify(req.body),
     webhookSignature,
     process.env.RAZOR_PAY_WEBHOOK_KEY
   );
+  console, log("isWebHookValid", isWebHookValid);
   if (!isWebHookValid) {
     return res
       .status(400)
@@ -66,27 +76,29 @@ router.post("/payment/webhook", async (req, res) => {
     orderId: paymentDetails.order_id,
   });
 
+  console.log("payment", payment);
+
   await payment.save();
 
   const user = await User.findOne({
     _id: payment.userId,
   });
 
+  console.log("user", user);
+
   user.isPremium = true;
   user.membershipType = payment?.notes?.memberShipType;
   await user.save();
 
-  if (req.body.event === "payment.captured") {
-  }
-  if (req.body.event === "payment.failed") {
-  }
+  // if (req.body.event === "payment.captured") {
+  // }
+  // if (req.body.event === "payment.failed") {
+  // }
 
-  return res
-    .status(200)
-    .json({
-      success: true,
-      message: "Webhook Received Successfully",
-    });
+  return res.status(200).json({
+    success: true,
+    message: "Webhook Received Successfully",
+  });
 });
 
 router.get("/payment/verify", userAuth, async (req, res) => {
