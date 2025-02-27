8e67091f04b330e6fb406c104e06edce6099cc73
burhanbitkraft <burhanuddin@bitkraft.co.in>
2025-02-27 22:39:57 +0530

    change limit

--- src/routes/user.js
+++ src/routes/user.js
@@ -62,7 +62,7 @@ router.get("/user/connections", userAuth, async (req, res) => {
 router.get("/user/feed", userAuth, async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    let limit = parseInt(req.query.limit) || 10;
+    let limit = parseInt(req.query.limit) || 30;
     limit = limit > 50 ? 50 : limit;
     const skip = (page - 1) * limit;
 
